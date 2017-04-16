0de62d598054d9fe5925b22a846c795fcc3fb86b
Tushar_bugskan <tushar@pratikar.com>
2017-04-16 09:48:11 +0530

    change in maze solver

--- maze_solver.py
+++ maze_solver.py
@@ -1,28 +1,40 @@
-class maze:
-    grid = []
-    def __init__(self, arry):
-        self.grid = arry
+maze = [[0, 0, 1, 0, 1],
+        [0, 1, 0, 0, 0],
+        [0, 1, 0, 1, 0],
+        [0, 0, 0, 1, 0],
+        [1, 1, 1, 0, 9]
+]
+from tabulate import tabulate
+width = len(maze)
+height = len(maze[0])
+print ('Maze width: %s, height %s' % (width, height))
 
-    def solve(self,x,y):
-        print (len(self.grid) )
-        if x == len(self.grid) - 1 and y == len(self.grid) - 1:
-            print("reached at end")
-            return True
-        if self.grid[x][y] == 1:
-            print("blocked at %d %d" %(x,y))
-            return False
-        if self.grid[x][y] == 2:
-            print("visited at %d %d " % (x,y))
-            return False
-        print("visiting %d %d " % (x,y))
-        self.grid[x][y] == 2
-        if ((x < len(self.grid) - 1 and self.solve(x + 1, y))
-            or (y>0 and self.solve(x,y-1))
-            or (x>0 and self.solve(x-1,y))
-            or (y< len(self.grid) - 1 and self.solve(x,y+1))):
-            return True
-        return False
 
-arry= input("enter maze")
-M1 = maze(arry)
-M1.solve(0,0)
+# noinspection PyGlobalUndefined
+def move(x, y):
+    global FoundWayOut
+    FoundWayOut = False
+    if FoundWayOut:
+        return
+
+    print(tabulate(maze))
+
+    if x < 0 or x > width - 1 or y < 0 or y > height - 1:
+        print('Out of bounds'); return
+    if maze[x][y] == 9:
+        FoundWayOut = True
+        print('Got it!'); return
+    if maze[x][y] == 1:
+        print('That is wall'); return
+    if maze[x][y] == 2:
+        print('Was here before'); return
+    maze[x][y] = 2
+
+    move(x + 1, y) # East
+    move(x, y + 1) # South
+    move(x - 1, y) # West
+    move(x, y - 1) # North
+
+move(0, 0)
+if not FoundWayOut:
+    print('There is no way out of this!')
